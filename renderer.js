509871408de8b7bde28fa8f44f70f9cb698972ee
vishlucario <vishlucario@users.noreply.github.com>
2018-06-08 15:08:14 -0700

    Added link to go to parent directory

--- renderer.js
+++ renderer.js
@@ -5,13 +5,20 @@ const fs = require("fs");
 
 const list = document.getElementById("file-list");
 
-const dir = "../"
-
+const dir = "../";
+let item = document.createElement("div");
+let link = document.createElement("a");
+let filePath = dir + "..";
+item.setAttribute('class', 'directory');
+link.setAttribute('href', filePath);
+link.appendChild(document.createTextNode('back'));
+item.appendChild(link);
+list.appendChild(item);
 fs.readdirSync(dir).forEach(file => {
-  let item = document.createElement("div");
-  let link = document.createElement("a");
-  let filePath = dir + file
-  item.setAttribute('class', fs.lstatSync(filePath).isDirectory() ? "directory" : "file");
+  item = document.createElement("div");
+  link = document.createElement("a");
+  filePath = dir + file;
+  item.setAttribute('class', fs.lstatSync(filePath).isDirectory() ? 'directory' : 'file');
   link.setAttribute('href', filePath);
   link.appendChild(document.createTextNode(file));
   item.appendChild(link);
