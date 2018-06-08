11ce68b1f01aa1f2866fc4f8343a8d58f3362cce
= <kirtanushah@gmail.com>
2018-06-08 11:33:20 -0700

    fix things

--- renderer.js
+++ renderer.js
@@ -9,9 +9,12 @@ const dir = "../"
 
 fs.readdirSync(dir).forEach(file => {
   let item = document.createElement("div");
+  let link = document.createElement("a");
   let filePath = dir + file
   item.setAttribute('class', fs.lstatSync(filePath).isDirectory() ? "directory" : "file");
-  item.innerHTML += "<ul> <a href = " + "filePath> " + file + " </a> </ul>"
+  link.setAttribute('href', filePath);
+  link.appendChild(document.createTextNode(file));
+  item.appendChild(link);
   list.appendChild(item);
 })
 
