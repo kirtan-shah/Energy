1faf48ca59bc28c6b4616b3377df0fa2926703de
= <kirtanushah@gmail.com>
2018-06-07 23:00:11 -0700

    list dirs and files rudimentarily

--- renderer.js
+++ renderer.js
@@ -1,7 +1,18 @@
 
 const fs = require("fs");
 
-const root = fs.readdirSync('/')
+//const root = fs.readdirSync('/')
+
+const list = document.getElementById("file-list");
+
+const dir = "/users/kirtan/desktop/"
+
+fs.readdirSync(dir).forEach(file => {
+  let item = document.createElement("div");
+  item.setAttribute('class', fs.lstatSync(dir + file).isDirectory() ? "directory" : "file");
+  item.appendChild(document.createTextNode(file));
+  list.appendChild(item);
+})
 
 console.log("Hello Renderer")
-console.log(root)
+//console.log(root)
