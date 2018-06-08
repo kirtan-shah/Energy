e202cc63a37f9d5deed2df8714f935ef57e748cf
= <kirtanushah@gmail.com>
2018-06-07 21:58:11 -0700

    add css

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
-require('babel-register');
-require('./main.js');  
+const electron = require('electron')
+
+const {app, BrowserWindow} = require('electron')
+
+// keep global ref to window to prevent GC
+let win
+
+function createWindow () {
+
+    win = new BrowserWindow({width: 800, height: 600})
+    win.loadFile('index.html')
+
+    // Open the DevTools.
+    //win.webContents.openDevTools()
+
+    win.on('closed', () => {
+        // dereference window objects
+        win = null
+    })
+}
+
+// after electron init
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // keep app open on macOS
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+})
+
+app.on('activate', () => {
+    // recreate window on macOS
+    if (win === null) {
+        createWindow()
+    }
+});
